5a78143e919554e4b49c77adf3fac7a0fc7151b2
Umaji Bagal <bagalumaji@gmail.com>
2025-07-26 11:41:08 +0530

    updated code with login page

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,16 +1,19 @@
 import pytest
 from playwright.async_api import expect
 
+from pages.login_page import LoginPage
+
 
 @pytest.mark.asyncio
 async def test_login_succeeds_with_valid_credentials(page):
+    username="standard_user"
+    password="secret_sauce"
+
     await page.goto("https://www.saucedemo.com/v1/")
-    user_name_text_box = page.locator("#user-name")
-    password_text_box = page.locator("#password")
-    login_button = page.locator('//input[@value="LOGIN"]')
-    await user_name_text_box.fill("standard_user")
-    await password_text_box.fill("secret_sauce")
-    await login_button.click()
+    login_page = LoginPage(page)
+    await login_page.enter_username(username)
+    await login_page.enter_password(password)
+    await login_page.click_on_login_button()
 
     await expect(page.get_by_text("Products")).to_be_visible()
     await page.wait_for_timeout(5000)
