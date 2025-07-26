328a9259f7cad0396499a611efe2fb666a9626dd
Umaji Bagal <bagalumaji@gmail.com>
2025-07-26 20:32:22 +0530

    test updated with product page method

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,6 +2,7 @@ import pytest
 from playwright.async_api import expect
 
 from pages.login_page import LoginPage
+from pages.product_page import ProductPage
 
 
 @pytest.mark.asyncio
@@ -12,5 +13,8 @@ async def test_login_succeeds_with_valid_credentials(page):
     await page.goto("https://www.saucedemo.com/v1/")
     login_page = LoginPage(page)
     await login_page.login_to_application(username, password)
-    await expect(page.get_by_text("Products")).to_be_visible()
+
+    product_page = ProductPage(page)
+    await product_page.verify_product_title_is_displayed()
+
     await page.wait_for_timeout(5000)
