944d739ac29a9708cecc4b5fc092fc4f4593fea3
Umaji Bagal <bagalumaji@gmail.com>
2025-07-26 10:17:47 +0530

    removed commented code

--- test_demo.py
+++ test_demo.py
@@ -3,12 +3,4 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_demo(page):
-    # pw = await async_playwright().start()
-    # browser = await pw.chromium.launch(headless=False)
-    # context = await browser.new_context()
-    # page = await context.new_page()
     await page.goto("https://www.google.com")
-    # await page.close()
-    # await context.close()
-    # await browser.close()
-    # await pw.stop()
