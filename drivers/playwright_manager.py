1cabccc4dc2618fd92bff1f57f1caadefa81a9a7
Umaji Bagal <bagalumaji@gmail.com>
2025-07-27 10:35:34 +0530

    implemented interface

--- drivers/playwright_manager.py
+++ drivers/playwright_manager.py
@@ -1,9 +1,10 @@
 from playwright.async_api import async_playwright, Playwright
+from drivers.interfaces.iplaywright_manager import IPlaywrightManager
 
 
-class PlaywrightManager:
-    async def start(self)->Playwright:
+class PlaywrightManager(IPlaywrightManager):
+    async def start(self) -> Playwright:
         return await async_playwright().start()
 
-    async def stop(self, playwright:Playwright)->None:
+    async def stop(self, playwright: Playwright) -> None:
         await playwright.stop()
