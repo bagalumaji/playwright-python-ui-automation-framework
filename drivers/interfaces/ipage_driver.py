df72e84540e2621314c6e2812c9a877651c2ef2c
Umaji Bagal <bagalumaji@gmail.com>
2025-07-27 10:27:20 +0530

    created ipage driver interface

---
+++ drivers/interfaces/ipage_driver.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+from playwright.async_api import Page, BrowserContext
+
+
+class IPageDriver(ABC):
+
+    @abstractmethod
+    async def create(self, context: BrowserContext) -> Page: ...
+
+    @abstractmethod
+    async def close(self, page: Page) -> None: ...
