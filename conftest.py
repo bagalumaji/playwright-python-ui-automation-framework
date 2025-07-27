e70893e6bfe168212a7186747a58882de0aa6c03
Umaji Bagal <bagalumaji@gmail.com>
2025-07-27 10:44:57 +0530

    updated code with interfaces

--- conftest.py
+++ conftest.py
@@ -5,37 +5,41 @@ from playwright.async_api import Playwright, Browser, BrowserContext, Page
 
 from drivers.browser_context_manager import BrowserContextManager
 from drivers.browser_manager import BrowserManager
-from drivers.page_driver import PageDriver
+from drivers.interfaces.ibrowser_context_manager import IBrowserContextManager
+from drivers.interfaces.ibrowser_manager import IBrowserManager
+from drivers.interfaces.ipage_manager import IPageManager
+from drivers.interfaces.iplaywright_manager import IPlaywrightManager
+from drivers.page_manager import PageManager
 from drivers.playwright_manager import PlaywrightManager
 
 
 @pytest_asyncio.fixture
-async def playwright()-> AsyncGenerator[Playwright, Any]:
-    manager = PlaywrightManager()
+async def playwright() -> AsyncGenerator[Playwright, Any]:
+    manager: IPlaywrightManager = PlaywrightManager()
     pw = await manager.start()
     yield pw
     await manager.stop(pw)
 
 
 @pytest_asyncio.fixture
-async def browser(playwright: Playwright)-> AsyncGenerator[Browser, Any]:
-    manager = BrowserManager()
+async def browser(playwright: Playwright) -> AsyncGenerator[Browser, Any]:
+    manager: IBrowserManager = BrowserManager()
     browser = await manager.launch(playwright)
     yield browser
     await manager.close(browser)
 
 
 @pytest_asyncio.fixture
-async def browser_context(browser: Browser)-> AsyncGenerator[BrowserContext, Any]:
-    manager = BrowserContextManager()
+async def browser_context(browser: Browser) -> AsyncGenerator[BrowserContext, Any]:
+    manager: IBrowserContextManager = BrowserContextManager()
     browser_context = await manager.create(browser)
     yield browser_context
     await manager.close(browser_context)
 
 
 @pytest_asyncio.fixture
-async def page(browser_context: BrowserContext)-> AsyncGenerator[Page, Any]:
-    driver = PageDriver()
+async def page(browser_context: BrowserContext) -> AsyncGenerator[Page, Any]:
+    driver: IPageManager = PageManager()
     new_page = await driver.create(browser_context)
     yield new_page
     await driver.close(new_page)
