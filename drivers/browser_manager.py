139ccc59bea31e62544fe2195ab9306d56475acf
umaji bagal <bagalumaji@gmail.com>
2025-07-30 10:53:45 +0530

    updated with browser possibilities

--- drivers/browser_manager.py
+++ drivers/browser_manager.py
@@ -1,20 +1,21 @@
 from playwright.async_api import Browser, Playwright
 
 from config.config_reader import ConfigReader
+from config.iconfig_reader import IConfigReader
 from drivers.interfaces.ibrowser_manager import IBrowserManager
 from enums.section_types import SectionTypes
 
 
 class BrowserManager(IBrowserManager):
     async def launch(self, playwright: Playwright) -> Browser:
-        config = ConfigReader()
-        browser = config.get(SectionTypes.DEFAULT, "browser")
+        config: IConfigReader = ConfigReader()
+        browser: str = config.get(SectionTypes.DEFAULT, "browser")
         headless_mode = config.getboolean(SectionTypes.DEFAULT, "headless")
 
         if browser.__eq__("chrome"):
             return await playwright.chromium.launch(headless=headless_mode, channel="chrome")
         elif browser.__eq__("edge"):
-            return await playwright.chromium.launch(headless=headless_mode,channel="msedge")
+            return await playwright.chromium.launch(headless=headless_mode, channel="msedge")
         elif browser.__eq__("firefox"):
             return await playwright.firefox.launch(headless=headless_mode)
         elif browser.__eq__("webkit"):
