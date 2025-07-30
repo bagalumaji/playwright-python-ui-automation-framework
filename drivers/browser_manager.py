8f91a40c0a386def6bddbede711408a77c22d5a2
umaji bagal <bagalumaji@gmail.com>
2025-07-30 10:52:24 +0530

    updated with browser possibilities

--- drivers/browser_manager.py
+++ drivers/browser_manager.py
@@ -8,15 +8,19 @@ from enums.section_types import SectionTypes
 class BrowserManager(IBrowserManager):
     async def launch(self, playwright: Playwright) -> Browser:
         config = ConfigReader()
-        browser = config.get(SectionTypes.DEFAULT,"browser")
-        headless_mode=config.getboolean(SectionTypes.DEFAULT,"headless")
-        if browser.__eq__("chromium"):
-            return await playwright.chromium.launch(headless=headless_mode)
+        browser = config.get(SectionTypes.DEFAULT, "browser")
+        headless_mode = config.getboolean(SectionTypes.DEFAULT, "headless")
+
+        if browser.__eq__("chrome"):
+            return await playwright.chromium.launch(headless=headless_mode, channel="chrome")
+        elif browser.__eq__("edge"):
+            return await playwright.chromium.launch(headless=headless_mode,channel="msedge")
         elif browser.__eq__("firefox"):
             return await playwright.firefox.launch(headless=headless_mode)
-        else:
+        elif browser.__eq__("webkit"):
             return await playwright.webkit.launch(headless=headless_mode)
-
+        else:
+            return await playwright.chromium.launch(headless=headless_mode)
 
     async def close(self, browser: Browser) -> None:
         await browser.close()
