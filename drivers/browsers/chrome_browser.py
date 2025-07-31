6f2c3c06934b24c936a5b29ac80d9fa40a26e75b
umaji bagal <bagalumaji@gmail.com>
2025-07-31 17:06:11 +0530

    created Chrome browser class

---
+++ drivers/browsers/chrome_browser.py
@@ -0,0 +1,8 @@
+from playwright.async_api import Playwright, Browser
+
+from drivers.ibrowser import IBrowser
+
+
+class ChromeBrowser(IBrowser):
+    async def create_browser(self, playwright: Playwright) -> Browser:
+        return await playwright.chromium.launch(headless=False, channel="chrome")
