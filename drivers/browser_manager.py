861738e252064ae34496fe2eaabc37070c07368a
umaji bagal <bagalumaji@gmail.com>
2025-07-31 17:06:40 +0530

    refactored class

--- drivers/browser_manager.py
+++ drivers/browser_manager.py
@@ -1,27 +1,20 @@
-from playwright.async_api import Browser, Playwright
+from playwright.async_api import Browser
 
 from config.config_reader import ConfigReader
 from config.iconfig_reader import IConfigReader
+from drivers.browser_factory import BrowserFactory
+from drivers.ibrowser import IBrowser
 from drivers.interfaces.ibrowser_manager import IBrowserManager
 from enums.section_types import SectionTypes
 
 
 class BrowserManager(IBrowserManager):
-    async def launch(self, playwright: Playwright) -> Browser:
+    async def launch(self) -> IBrowser:
         config: IConfigReader = ConfigReader()
-        browser: str = config.get(SectionTypes.DEFAULT, "browser")
+        browser_name = config.get(SectionTypes.DEFAULT, "browser").lower()
         headless_mode = config.getboolean(SectionTypes.DEFAULT, "headless")
+        return  BrowserFactory().get_browser(browser_name)
 
-        if browser.__eq__("chrome"):
-            return await playwright.chromium.launch(headless=headless_mode, channel="chrome")
-        elif browser.__eq__("edge"):
-            return await playwright.chromium.launch(headless=headless_mode, channel="msedge")
-        elif browser.__eq__("firefox"):
-            return await playwright.firefox.launch(headless=headless_mode)
-        elif browser.__eq__("webkit"):
-            return await playwright.webkit.launch(headless=headless_mode)
-        else:
-            return await playwright.chromium.launch(headless=headless_mode)
 
     async def close(self, browser: Browser) -> None:
         await browser.close()
