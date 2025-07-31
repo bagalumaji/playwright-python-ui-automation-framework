f05846940872c8230f1459baab6410412915fe39
umaji bagal <bagalumaji@gmail.com>
2025-07-31 17:04:57 +0530

    refactored

--- drivers/interfaces/ibrowser_manager.py
+++ drivers/interfaces/ibrowser_manager.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
 
-from playwright.async_api import Playwright, Browser
+from playwright.async_api import Browser
+
+from drivers.ibrowser import IBrowser
 
 
 class IBrowserManager(ABC):
 
     @abstractmethod
-    async def launch(self, playwright: Playwright) -> Browser: ...
+    async def launch(self) -> IBrowser: ...
 
     @abstractmethod
     async def close(self, browser: Browser) -> None: ...
